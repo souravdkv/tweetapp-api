74d4778add8ae3328d11792f79f1ac9d11403a5e
Sourav Dutta <Sourav@DESKTOP-LM0O3QE>
2021-06-12 20:45:33 +0530

    tweet sorting bugfix

--- src/main/java/com/tweetapp/main/service/impl/TweetServiceImpl.java
+++ src/main/java/com/tweetapp/main/service/impl/TweetServiceImpl.java
@@ -120,10 +120,6 @@ public class TweetServiceImpl implements TweetService {
 				.add(TweetsResponse.builder().id(tweet.getId()).name(tweet.getFirstName() + " " + tweet.getLastName())
 						.tweets(tweet.getTweet()).likes(tweet.getLikes()).replies(tweet.getReplies()).build()));
 
-		// sort the tweets by name
-		Comparator<TweetsResponse> tweetsResponseComparator = (t1, t2) -> t1.getName().compareTo(t2.getName());
-		tweetsResponse.sort(tweetsResponseComparator);
-
 		return tweetsResponse;
 	}
 
