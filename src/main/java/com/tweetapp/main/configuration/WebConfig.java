7e78c35dcb9e13c9acd8884e5c6d8fad1e354e55
Sourav Dutta <Sourav@DESKTOP-LM0O3QE>
2021-06-14 12:33:10 +0530

    cors changes

--- src/main/java/com/tweetapp/main/configuration/WebConfig.java
+++ src/main/java/com/tweetapp/main/configuration/WebConfig.java
@@ -11,24 +11,24 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+//@Configuration
+public class WebConfig {
 
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-		List<MediaType> list = new ArrayList<>();
-		list.add(MediaType.APPLICATION_JSON);
-		list.add(new MediaType("text", "html", StandardCharsets.UTF_8));
-		list.add(new MediaType("application", "*+json", StandardCharsets.UTF_8));
-		converter.setSupportedMediaTypes(list);
-		converters.add(converter);
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowCredentials(true);
-	}
+//	@Override
+//	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+//		List<MediaType> list = new ArrayList<>();
+//		list.add(MediaType.APPLICATION_JSON);
+//		list.add(new MediaType("text", "html", StandardCharsets.UTF_8));
+//		list.add(new MediaType("application", "*+json", StandardCharsets.UTF_8));
+//		converter.setSupportedMediaTypes(list);
+//		converters.add(converter);
+//	}
+//
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+//				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowCredentials(true);
+//	}
 
 }
