39a645d9b0e45104730af7f8d4705a992207ba81
Sourav Dutta <Sourav@DESKTOP-LM0O3QE>
2021-06-14 12:28:45 +0530

    CORS config changes

--- src/main/java/com/tweetapp/main/controller/TweetController.java
+++ src/main/java/com/tweetapp/main/controller/TweetController.java
@@ -23,6 +23,7 @@ import javax.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,6 +47,7 @@ import com.tweetapp.main.payload.response.TweetsResponse;
 import com.tweetapp.main.payload.response.UsersResponse;
 import com.tweetapp.main.service.TweetService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping(TWEETS_API_PATH)
 public class TweetController {
